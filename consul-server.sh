a7e6f3e8a2f3d55f56e01739e45c7c25dc47d852
anandg112 <anandg112@gmail.com>
2019-08-19 12:23:51 -0400

    add consul client

--- consul-server.sh
+++ consul-server.sh
@@ -5,6 +5,10 @@ docker pull consul
 
 
 echo "running consul agent"
+SERVER_CONTAINER_NAME=$1
+echo "${SERVER_CONTAINER_NAME}"
+CLIENT_CONTAINER_NAME=$2
+echo "${CLIENT_CONTAINER_NAME}"
 
 # process will run in the background. 
 # also set port mapping to your local machine as 
@@ -15,9 +19,21 @@ if [ ! "$(docker ps -a | grep badger)" ]; then
     -d \
     -p 8500:8500 \
     -p 8600:8600/udp \
-    --name=badger \
+    --name=$SERVER_CONTAINER_NAME \
     consul agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
 else 
     echo " container already running!"
+fi
 
+SERVER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $SERVER_CONTAINER_NAME)
+echo "${SERVER_IP}"
+
+echo "running Consul client"
+if [ ! "$(docker ps -a | grep fox)" ]; then
+    echo "client container not running, starting consul"
+    docker run \
+    --name=$CLIENT_CONTAINER_NAME \
+    consul agent -node=client-1 -join=$SERVER_IP
+else 
+    echo " container already running!"
 fi
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 
 #### Execute shell script
 
-```./consul-server.sh```
+```./consul-server.sh consul-server consul-client```
 
 
 #### Running on localhost
@@ -11,3 +11,7 @@
 Accesible on:
 
 ```localhost:8500```
+
+IP address of the consul server: 
+```docker exec badger consul members```
+
